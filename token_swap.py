aadbab1b43e52b78f7fec41e7099674908bd154d
Alex Scrobot <scrobot91@gmail.com>
2023-03-24 14:22:30 +0100

    get_max_tokens

--- token_swap.py
+++ token_swap.py
@@ -1,27 +1,32 @@
 from collections import defaultdict
 import heapq
 
+DECIMALS: int = 6
 
-def get_max_tokens(tokens, n, queries, q):
+
+def get_max_tokens(tokens: list[str], token_pairs: int, queries: list[str], queries_count: int):
+    _validate(tokens)
+    _validate(queries)
+
     # Create a graph with exchange rates as edges
     graph = defaultdict(dict)
-    for i in range(n):
-        tokenA, tokenB, rate = tokens[i].split(', ')
+    for i in range(token_pairs):
+        token_a, token_b, rate = tokens[i].split(', ')
         rate = float(rate)
-        graph[tokenA][tokenB] = rate
-        graph[tokenB][tokenA] = 1.0 / rate
+        graph[token_a][token_b] = rate
+        graph[token_b][token_a] = 1.0 / rate
 
     # Run Dijkstra's algorithm for each query
     max_tokens = []
-    for i in range(q):
-        tokenA, tokenC, amount = queries[i].split(', ')
+    for i in range(queries_count):
+        token_a, token_c, amount = queries[i].split(', ')
         amount = float(amount)
-        dist = {tokenA: amount}
-        heap = [(amount, tokenA)]
+        dist = {token_a: amount}
+        heap = [(amount, token_a)]
         while heap:
             curr_amount, curr_token = heapq.heappop(heap)
-            if curr_token == tokenC:
-                max_tokens.append(round(curr_amount, 6))
+            if curr_token == token_c:
+                max_tokens.append(round(curr_amount, DECIMALS))
                 break
             for neighbor, rate in graph[curr_token].items():
                 next_amount = curr_amount * rate
@@ -29,6 +34,11 @@ def get_max_tokens(tokens, n, queries, q):
                     dist[neighbor] = next_amount
                     heapq.heappush(heap, (next_amount, neighbor))
 
-    print(max_tokens)
-
     return max_tokens
+
+
+def _validate(tokens: list[str]):
+    for token in tokens:
+        token_a, token_b, rate = token.split(', ')
+        assert token_a != token_b
+        assert float(rate) > 0
